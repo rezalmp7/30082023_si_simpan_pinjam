fbcb44f216ca35de0463118f980e7832fdbac16d
rezalmp7 <rezalwahyu10@gmail.com>
2023-09-17 16:12:04 +0700

    Angsuran

--- application/controllers/Angsuran.php
+++ application/controllers/Angsuran.php
@@ -32,22 +32,75 @@ class Angsuran extends CI_Controller {
 		$this->load->view('admin/layout/footer');
 	}
     
+    private function beetweenMonth2Date($date1Val, $date2Val) {
+        $date1 = date("Y-m-d", strtotime($date1Val));
+        $date2 = date("Y-m-d", strtotime($date2Val));
+
+        $year1 = date('Y', strtotime($date1));
+        $year2 = date('Y', strtotime($date2));
+
+        $month1 = date('m', strtotime($date1));
+        $month2 = date('m', strtotime($date2));
+
+        $diff = (($year2 - $year1) * 12) + ($month2 - $month1);
+
+        return $diff;
+    } 
+
 	public function tambah() {
+        // echo "<pre>";
         $get = $this->input->get();
 
 		$data['nasabah'] = $this->db->get("nasabah")->result();
 		
         if(isset($get['nasabah'])) {
-            $data['pinjaman_select'] = $this->db->get_where("pinjaman", array('id_nasabah' => $get['nasabah']))->row();
-            $data['nasabah_select'] = $this->db->get_where("nasabah", array('id_nasabah' => $get['nasabah']))->row();
+            $pinjamanSelect = $this->db->get_where("pinjaman", array('id_nasabah' => $get['nasabah'], 'status' => 0))->result();
+            $data['nasabahSelect'] = $this->db->get_where("nasabah", array('id_nasabah' => $get['nasabah']))->row();
+
+            $jumlah_angsuran = 0;
+
+            $angsuranPerPinjaman = array();
+
+            // $tanggalIni = date('Y-m-1', strtotime("2023--1"));
+            $tanggalIni = date('Y-m-1');
+            
+            foreach ($pinjamanSelect as $key => $value) {                
+                $cekAngsuranSelesaiQuery = $this->db->select("*")
+                    ->from('angsuranpaid')
+                    ->where('id_pinjaman', $value->id_pinjaman)
+                    ->order_by('angsuran', 'DESC')
+                    ->get();
+                $cekAngsuranSelesaiData = $cekAngsuranSelesaiQuery->last_row();
+                
+                if($cekAngsuranSelesaiData) {
+                    $angsuranKe = $cekAngsuranSelesaiData->angsuran+1;
+                    $angsuranPerPinjaman[] = array(
+                        'data_pinjaman' => $value,
+                        'angsuran' => $angsuranKe,
+                        'angsuran_bulan' => date("M Y", strtotime("+".$angsuranKe."month", strtotime($value->tgl_pinjaman))),
+                        'nominal_angsuran' => $value->nominal_angsuran + ($value->nominal_setuju*$value->bunga/100)/12
+                    );
+                } else {
+                    //cek jika telat
+                    $angsuranKe = 1;
+                    $angsuranBulan = date("M Y", strtotime("+".$angsuranKe."month", strtotime($value->tgl_pinjaman)));
+                    $angsuranPerPinjaman[] = array(
+                        'data_pinjaman' => $value,
+                        'angsuran' => $angsuranKe,
+                        'angsuran_bulan' => $angsuranBulan,
+                        'isTelat' => date('Ym', strtotime($tanggalIni)) <= date('Ym', strtotime($angsuranBulan)) ? 0 : 1,
+                        'nominal_angsuran' => $value->nominal_angsuran + ($value->nominal_setuju*$value->bunga/100)/12
+                    );
+                }
+            }
         }
 
-        echo "<pre>";
-        print_r($data);
+        $data['pinjamanSelect'] = $pinjamanSelect;
 
-		// $this->load->view('admin/layout/header');
-		// $this->load->view('admin/angsuran/tambah', $data);
-		// $this->load->view('admin/layout/footer');
+        $data['angsuranPerPinjaman'] = $angsuranPerPinjaman;
+		$this->load->view('admin/layout/header');
+		$this->load->view('admin/angsuran/tambah', $data);
+		$this->load->view('admin/layout/footer');
 	}
     public function upload_foto_nasabah($input_file_photo, $namaFile) {
         $config['upload_path']          = './assets/foto_nasabah_peminjam/';
--- application/views/admin/angsuran/tambah.php
+++ application/views/admin/angsuran/tambah.php
@@ -1,7 +1,7 @@
 
 <div class="row mt-4">
     <div class="col-12 mb-lg-0 mb-4">
-        <div class="card z-index-2 h-100">
+        <div class="card z-index-2">
             <div class="card-header pb-0 pt-3 bg-transparent">
                 <h6 class="text-capitalize">Pilih Untuk Pengecekan Angsuran Nasabah</h6>
             </div>
@@ -9,13 +9,13 @@
                 <form method="GET" action="<?php echo base_url(); ?>angsuran/tambah">
                     <div class="form-group">
                         <label for="example-text-input" class="form-control-label">Nasabah</label>
-                        <select class="select_2_class form-control" name="nasabah" required>
+                        <select class="select_2_class col-12 form-control" name="nasabah" required>
                             <option>-- Pilih Nasabah --</option>
                             <?php
                             print_r($nasabah);
                             foreach ($nasabah as $key => $value) {
                             ?>
-                            <option value="<?php echo $value->id_nasabah; ?>"><?php echo $value->nama; ?></option>
+                            <option value="<?php echo $value->id_nasabah; ?>" <?php if($nasabahSelect->id_nasabah == $value->id_nasabah) { echo "selected"; } ?>><?php echo $value->nama; ?></option>
                             <?php
                             }
                             ?>
@@ -28,4 +28,80 @@
             </div>
         </div>
     </div>
+    <div class="col-12 mb-lg-0 mb-4">
+        <div class="card z-index-2 h-100 mt-5">
+            <div class="card-header pb-0 pt-3 bg-transparent">
+                <h6 class="text-capitalize">Angsuran</h6>
+            </div>
+            <div class="card-body p-3">
+                <h5>Tagihan Terakhir Belum Terbayarkan</h5>
+                <?php
+                foreach ($angsuranPerPinjaman as $key => $value) {
+                ?>
+                <div class="list-group-item border-0 p-3 d-flex bg-gray-300 justify-content-between mb-2 border-radius-lg">
+                    <div class="d-flex align-items-center">
+                        <?php if($value['isTelat'] == 1) { ?>
+                        <button class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-exclamation-circle"></i></button>
+                        <?php } else { ?>
+                        <button class="btn btn-icon-only btn-rounded btn-outline-warning mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-hourglass-half"></i></button>
+                        <?php } ?>
+                        <div class="d-flex flex-column">
+                            <h6 class="mb-1 text-dark text-sm">Pinjaman <?php echo date("d/m/Y", strtotime($value['data_pinjaman']->tgl_pinjaman)); ?></h6>
+                            <h6 class="mb-1 text-dark text-sm">Angsuran #<?php echo $value['angsuran']; ?></h6>
+                            <span class="text-xs"><?php echo date("M Y", strtotime($value['angsuran_bulan'])); ?></span>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center text-primary text-gradient text-sm font-weight-bold">
+                        Rp <?php echo number_format($value['nominal_angsuran']); ?>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+                <form method="POST" action="<?php echo base_url(); ?>angsuran/store">
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Pinjaman</label>
+                        <select class="select_2_class col-12 form-control" name="nasabah" required>
+                            <option>-- Pilih Pinjaman --</option>
+                            <?php
+                            foreach ($pinjamanSelect as $key => $value) {
+                            ?>
+                            <option value="<?php echo $value->id_pinjaman; ?>">Pinjaman <?php echo date("d/m/Y", strtotime($value->tgl_pinjaman)); ?> - Rp <?php echo number_format($value->nominal_setuju); ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Nominal Angsuran</label>
+                        <input class="form-control" type="number" name="nominal_angsuran" placeholder="20000000" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Terlambat Pembayaran</label>
+                        <input class="form-control" type="number" name="terlambat_pembayaran" id="input_nominal_disetujui" placeholder="20000000" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Total Yang Harus Dibayar</label>
+                        <input class="form-control" type="number" name="total_bayar" id="total_bayar" placeholder="Nominal Angsuran" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Tanggal Pembayaran Angsuran</label>
+                        <input class="form-control" type="date" name="tgl_pembayaran" id="tgl_pembayaran" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Tahap Angsuran Ke-</label>
+                        <input class="form-control" type="number" name="thp_angsuran" id="thp_angsuran" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Denda</label>
+                        <input class="form-control" type="number" name="denda" id="denda" required>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success btn-sm" type="submit">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+<div class="clearfix"></div>
